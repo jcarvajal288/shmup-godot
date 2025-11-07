355ccaf2dfeb0fbd2aa1c37942ed36a9d2615877
Max Carvajal <jcarvajal288@gmail.com>
2025-11-07 08:12:42 -0600

    Animate movement

--- Characters/Player/player.gd
+++ Characters/Player/player.gd
@@ -3,14 +3,23 @@ class_name Player extends CharacterBody2D
 var speed = 150
 
 func _physics_process(_delta: float) -> void:
-	movement()
+	move()
+	animate()
 
 
-func movement() -> void:
+func move() -> void:
 	var movement_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	var snapped_vector = Vector2(
 		snapped(movement_vector.x, 0.5), 
 		snapped(movement_vector.y, 0.5)
-	)
+	).normalized()
 	velocity = snapped_vector * speed
 	move_and_slide()
+
+
+func animate() -> void:
+	if velocity.x != 0:
+		$AnimationPlayer.play("side")
+	else:
+		$AnimationPlayer.play("standard")
+	$Sprite2D.flip_h = velocity.x > 0
