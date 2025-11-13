671793817ea0c2b606fd984d1b64062eafdfb0fb
Max Carvajal <jcarvajal288@gmail.com>
2025-11-12 22:02:11 -0600

    Enemies disappear when hit

--- Components/Hurtbox/hurtbox.gd
+++ Components/Hurtbox/hurtbox.gd
@@ -1,1 +1,12 @@
 extends Area2D
+
+@export var target: Global.CollisionLayer = Global.CollisionLayer.SHOT
+
+
+func _ready() -> void:
+	set_collision_layer_value(target, true)
+
+
+func _on_area_entered(_area: Area2D) -> void:
+	get_parent().queue_free()
+
