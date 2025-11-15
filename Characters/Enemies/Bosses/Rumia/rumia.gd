df3b8cbd955b7f56e767bb7cbb09268a63437f0f
Max Carvajal <jcarvajal288@gmail.com>
2025-11-15 13:48:57 -0600

    Basic Rumia movement animation

--- Characters/Enemies/Bosses/Rumia/rumia.gd
+++ Characters/Enemies/Bosses/Rumia/rumia.gd
@@ -1,1 +1,8 @@
 extends Enemy
+
+func _physics_process(_delta: float) -> void:
+	super(_delta)
+	if velocity.length() > 1.0:
+		$AnimationPlayer.play("moving")
+	elif $AnimationPlayer.current_animation == "moving":
+		$AnimationPlayer.play("moveTransition")
