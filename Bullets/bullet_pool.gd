85223e4d88a8fad49be579572ccccc8d94bfca14
Max Carvajal <jcarvajal288@gmail.com>
2025-11-18 22:00:44 -0600

    Update GarbageCollector to collect the new Area2D bullets

--- Bullets/bullet_pool.gd
+++ Bullets/bullet_pool.gd
@@ -9,25 +9,6 @@ func _ready() -> void:
 		pool[bullet_type] = []
 
 
-# func fill_pool() -> void:
-# 	print('filling')
-# 	var batch_size = 20
-# 	var count = 0
-# 	var bullet_types = [Global.BulletType.CIRCLE]
-# 	for bullet_type in bullet_types:
-# 		pool[bullet_type] = []
-# 		print(bullet_type)
-# 		for _i in range(POOL_SIZE):
-# 			var bullet = create_bullet(bullet_type)
-# 			pool[bullet_type].push_back(bullet)
-# 			count += 1
-# 			if count >= batch_size:
-# 				count = 0
-# 				await get_tree().process_frame
-# 		await Global.wait_for_sec(1.0)
-# 	print('fill ended')
-
-
 func create_bullet(bullet_type: Global.BulletType) -> Bullet:
 	var bullet = Global.BULLET_SCENES[bullet_type].instantiate()
 	bullet.global_position = Vector2(-100, -100)
@@ -39,7 +20,6 @@ func create_bullet(bullet_type: Global.BulletType) -> Bullet:
 func get_bullet(bullet_type: Global.BulletType) -> Bullet:
 	var bullet
 	if pool[bullet_type].is_empty():
-		print('pool empty')
 		bullet = create_bullet(bullet_type)
 	else:
 		bullet = pool[bullet_type].pop_back()
--- Components/GarbageCollector/garbage_collector.gd
+++ Components/GarbageCollector/garbage_collector.gd
@@ -6,3 +6,8 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 		BulletPool.put_bullet(body)
 	else:
 		body.queue_free()
+
+
+func _on_area_exited(area: Area2D) -> void:
+	if area is Bullet:
+		BulletPool.put_bullet(area)
