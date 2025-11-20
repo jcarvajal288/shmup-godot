e65b3351dec93d10f116228547473f3590ccea50
Max Carvajal <jcarvajal288@gmail.com>
2025-11-20 06:40:47 -0600

    Give Rumia Health and a Hitbox

--- Characters/Enemies/Components/Health/health.gd
+++ Characters/Enemies/Components/Health/health.gd
@@ -1,11 +1,13 @@
 extends Node
 
-var current_health = 5
+@export var max_health = 5
+@onready var current_health = max_health
 
 signal on_death
 
 func _on_hit() -> void:
 	current_health -= 1
+	print(current_health)
 	if current_health <= 0:
 		on_death.emit()
 
