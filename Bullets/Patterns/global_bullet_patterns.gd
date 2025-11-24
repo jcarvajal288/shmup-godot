d6bb083a00e8acc2a1a1dff43fa692dd1485ef0d
Max Carvajal <jcarvajal288@gmail.com>
2025-11-24 07:48:34 -0600

    Rename Spiral to FireSpiral

--- Bullets/Patterns/global_bullet_patterns.gd
+++ Bullets/Patterns/global_bullet_patterns.gd
@@ -2,4 +2,4 @@ extends Node
 
 const FIRE_STRAIGHT: PackedScene = preload("res://Bullets/Patterns/FireStraight/FireStraight.tscn")
 const FIRE_ARC: PackedScene = preload("res://Bullets/Patterns/FireArc/FireArc.tscn")
-const SPIRAL: PackedScene = preload("res://Bullets/Patterns/Spiral/Spiral.tscn")
+const FIRE_SPIRAL: PackedScene = preload("res://Bullets/Patterns/FireSpiral/FireSpiral.tscn")
