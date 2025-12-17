d279f707bee9e33875b13de46578c7294fa3b430
Max Carvajal <jcarvajal288@gmail.com>
2025-12-16 20:06:58 -0600

    Remove debugger error from fire_straight.gd

--- Bullets/Patterns/FireStraight/fire_straight.gd
+++ Bullets/Patterns/FireStraight/fire_straight.gd
@@ -4,5 +4,4 @@ class_name FireStraight extends BulletPattern
 func shoot() -> void:
 	var bullet = BulletPool.get_bullet(bullet_type)
 	bullet.build(bullet_color, global_position, direction, speed)
-	get_tree().root.add_child(bullet)	
 	do_repetitions()
