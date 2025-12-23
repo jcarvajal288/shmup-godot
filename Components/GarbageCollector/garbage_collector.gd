148c598efd322f00176c795a8d0541600ff29803
Max Carvajal <jcarvajal288@gmail.com>
2025-12-22 23:44:54 -0600

    Remove bullet patterns from enemies leaving the play area

--- Components/GarbageCollector/garbage_collector.gd
+++ Components/GarbageCollector/garbage_collector.gd
@@ -1,6 +1,10 @@
 extends Node2D
 
 
+func _ready() -> void:
+	$PlayArea.body_exited.connect(_on_play_area_body_exited)
+
+
 func _on_area_2d_body_exited(body: Node2D) -> void:
 	if body is Bullet:
 		BulletPool.put_bullet(body)
@@ -11,3 +15,10 @@ func _on_area_2d_body_exited(body: Node2D) -> void:
 func _on_area_exited(area: Area2D) -> void:
 	if area is Bullet:
 		BulletPool.put_bullet(area)
+
+
+func _on_play_area_body_exited(body: Node2D) -> void:
+	print("in exit play area")
+	if body is Enemy and body.bullet_pattern != null:
+		print("disabling firing")
+		body.bullet_pattern.queue_free()
